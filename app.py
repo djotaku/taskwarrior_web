46d2823d43c005c20890b4272cdb8488a1bda570
Eric Mesa <ericsbinaryworld@gmail.com>
2023-11-16 21:37:22 -0500

    Adding jinja_partials
    Right now it's not doing much, but that's because I haven't truly started using HTMX yet.

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import render_template
+import jinja_partials
 import task
 
 app = Flask(__name__)
 
-
+jinja_partials.register_extensions(app)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+@app.route('/tasks')
+def tasks():
+    tasks = task.task_list()
+    return render_template('tasks.html', tasks=tasks)
 
 @app.route('/due-today')
 def due_today():
