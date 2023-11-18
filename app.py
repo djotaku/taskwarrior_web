f04ecd99b8593c4734a1eb909095a84670f535ca
Eric Mesa <ericsbinaryworld@gmail.com>
2023-11-17 21:03:46 -0500

    The Basics of the view are nearly done
    Right now it's read-only, but I've got the framework completed

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 from flask import render_template
 import jinja_partials
@@ -6,20 +8,35 @@ import task
 app = Flask(__name__)
 
 jinja_partials.register_extensions(app)
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/tasks')
 def tasks():
-    tasks = task.task_list()
-    return render_template('tasks.html', tasks=tasks)
+    starting_tasks = task.task_list_pending(None)
+    return render_template('tasks.html', tasks=starting_tasks)
+
+@app.route('/overdue')
+def overdue():
+    overdue_tasks = task.task_list_overdue()
+    return render_template('partials/task_table.html', tasks=overdue_tasks)
 
 @app.route('/due-today')
 def due_today():
     """The tasks that have a due date of today."""
-    tasks = task.task_list()
-    return render_template('due_today.html', tasks=tasks)
+    today = datetime.today()
+    today_tasks = task.task_list_pending(today)
+    return render_template('partials/task_table.html', tasks=today_tasks)
+
+
+@app.route('/all-incomplete')
+def all_incomplete():
+    all_incomplete_tasks = task.task_list_pending(None)
+    return render_template('partials/task_table.html', tasks=all_incomplete_tasks)
 
 
 if __name__ == '__main__':
--- task.py
+++ task.py
@@ -1,14 +1,25 @@
+from datetime import datetime
+
 from taskwarrior import Client
 
 client = Client()
 
 
-# tasks = client.filter(status="pending")
-
-# for task in tasks:
-#    print(task)
-
-def task_list():
+def task_list_pending(due_date_start: datetime, due_date_end: datetime | None = None):
     """Return a list of tasks. Will add more features to this eventually for the sake of DRY. """
-    tasks = client.filter(status="pending", due="today")
-    return tasks
+    tasks = client.filter(status="pending")
+    if due_date_start and due_date_end:
+        pass
+    elif due_date_start:
+        filtered_tasks = [task for task in tasks if task.due is not None]  # filter out tasks without due dates
+        return [task for task in filtered_tasks if
+                task.due.year == due_date_start.year and task.due.month == due_date_start.month and task.due.day == due_date_start.day]
+    else:
+        return tasks
+
+
+def task_list_overdue():
+    tasks = client.filter(status="pending")
+    filtered_tasks = [task for task in tasks if task.due is not None]  # filter out tasks without due dates
+    present = datetime.now().astimezone()
+    return [task for task in filtered_tasks if task.due < present]
