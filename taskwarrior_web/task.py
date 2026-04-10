9f3578de84b867f70a6f4fe380fb7b0abb7d01e1
Eric Mesa <ericsbinaryworld@gmail.com>
2026-04-09 21:07:44 -0400

    debugging

--- taskwarrior_web/task.py
+++ taskwarrior_web/task.py
@@ -72,6 +72,7 @@ def add_task(task_description: str, task_project: str, tags: str, due_date: date
     operations = Operations()
     task_uuid = str(uuid.uuid4())
     task = replica.create_task(task_uuid, operations)
+    replica.commit_operations(operations)
     task.set_description(task_description, operations)
     task.set_entry(datetime.now(timezone.utc), operations)
     task.set_value("project", task_project, operations)
