a30d1880d53dc10ad4e212012a1d05010bf832af
Eric Mesa <ericsbinaryworld@gmail.com>
2026-04-15 21:39:35 -0400

    almost there with the container

---
+++ Container/create_image_fedora_part_2.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+echo "Build from localhost/tw_p1"
+ctr=$(buildah from localhost/tw_p1)
+
+buildah run $ctr /bin/sh -c 'git clone https://github.com/djotaku/taskwarrior_web.git'
+
+echo "Create the virtual environment"
+buildah run $ctr /bin/sh -c 'python -m venv /taskwarrior_web/venv'
+
+echo "Activate the virtual environment"
+buildah run $ctr /bin/bash -c 'source /taskwarrior_web/venv/bin/activate'
+
+echo "Install the requirements and Gunicorn"
+
+buildah run $ctr /bin/sh -c 'cd /taskwarrior_web'
+buildah run $ctr /bin/sh -c '/usr/sbin/python3.13 -m ensurepip --upgrade'
+buildah run $ctr /bin/sh -c 'python3.13 -m pip install -r /taskwarrior_web/requirements.txt'
+buildah run $ctr /bin/sh -c 'python3.13 -m pip install gunicorn'
+
+buildah run $ctr /bin/sh -c 'touch /root/.taskrc'
+
+echo "Set the volumes"
+buildah config --volume /taskwarrior_web/ $ctr
+buildah config --volume /root/.task/ $ctr
+buildah config --volume /root/.taskrc $ctr
+
+echo "set the timezone"
+buildah run $ctr /bin/bash -c 'ln -snf /usr/share/zoneinfo/America/New_York /etc/localtime && echo America/New_York > /etc/timezone'
+
+echo "set the command to run"
+buildah config --cmd "gunicorn --chdir taskwarrior_web/src/ -b 0.0.0.0:8000 app:app --reload" $ctr
+
+echo "commit the image"
+buildah commit $ctr taskwarrior_web
