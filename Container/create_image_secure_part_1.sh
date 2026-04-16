d562ed9f32156a8215c6f9853b96a47f3063b036
Eric Mesa <ericsbinaryworld@gmail.com>
2026-04-15 20:04:48 -0400

    grabbing the taskwarrior v3 which is not in regular debian 13

--- Container/create_image_secure_part_1.sh
+++ Container/create_image_secure_part_1.sh
@@ -6,9 +6,13 @@ ctr=$(buildah from dhi.io/python:3.13-debian13-dev)
 echo "Upgrade pip"
 buildah run $ctr /bin/bash -c 'pip install -U pip'
 
+echo "Turn on Backports to get newer taskwarrior"
+buildah copy $ctr 'debian-backports.sources' '/etc/apt/sources.list.d/debian-backports.sources'
+
 echo "Install taskwarrior, git, and m"
 buildah run $ctr /bin/bash -c 'apt update'
-buildah run $ctr /bin/bash -c 'apt install taskwarrior git gcc -y'
+buildah run $ctr /bin/bash -c 'apt install git gcc -y'
+buildah run $ctr /bin/bash -c 'apt install taskwarrior/trixie-backports'
 
 echo "commit the image"
 buildah commit $ctr tw_p1
